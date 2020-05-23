5ce2e11f56b82e20d1ae580436fecaf21db9fb4d
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-23 11:15:54 +0200

    feature: Sockets in crud #31 - allow annonimus insert in reposiitory

--- libs/shared/mongo/src/lib/repositories/item.repository.ts
+++ libs/shared/mongo/src/lib/repositories/item.repository.ts
@@ -320,7 +320,7 @@ export class MongoItemRepository<
 
     result["__info"] = {
       create: {
-        username: user.username,
+        username: user.username ? user.username : null,
         date: new Date()
       }
     };
@@ -350,7 +350,7 @@ export class MongoItemRepository<
     result["__info"] = {
       ...info,
       update: {
-        username: user.username,
+        username: user.username ? user.username : null,
         date: new Date()
       }
     };
