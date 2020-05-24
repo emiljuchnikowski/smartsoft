1d10c27b3b1b2bc3b718756157eb7199f6fda9b1
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-24 02:09:48 +0200

    refactoring: Remove data persister in auth ngrx

--- libs/auth/shell/angular/src/lib/+state/auth.effects.ts
+++ libs/auth/shell/angular/src/lib/+state/auth.effects.ts
@@ -1,104 +1,129 @@
-import {Injectable, Optional} from "@angular/core";
-import { createEffect, Actions } from "@ngrx/effects";
-import { DataPersistence } from "@nrwl/angular";
-import { map } from "rxjs/operators";
+import { Injectable, Optional } from "@angular/core";
+import { createEffect, Actions, ofType } from "@ngrx/effects";
+import { pessimisticUpdate } from "@nrwl/angular";
+import { map, tap } from "rxjs/operators";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 
-import * as fromAuth from "./auth.reducer";
 import * as AuthActions from "./auth.actions";
 import { AuthService } from "../services/auth/auth.service";
 
 @Injectable()
 export class AuthEffects {
   initToken$ = createEffect(() =>
-    this.dataPersistence.fetch(AuthActions.initToken, {
-      run: () => {
-        return AuthActions.initTokenSuccess({
-          token: this.service.token,
-          username: this.service.username
-        });
-      },
+    this.actions$.pipe(
+      ofType(AuthActions.initToken.type),
+      pessimisticUpdate({
+        run: () => {
+          return AuthActions.initTokenSuccess({
+            token: this.service.token,
+            username: this.service.username
+          });
+        },
 
-      onError: (action: ReturnType<typeof AuthActions.initToken>, error) => {
-        console.error("Error", error);
-        return AuthActions.initTokenFailure({ error });
-      }
-    })
+        onError: (action: ReturnType<typeof AuthActions.initToken>, error) => {
+          console.error("Error", error);
+          return AuthActions.initTokenFailure({ error });
+        }
+      })
+    )
   );
 
-  createToken$: Observable<any> = createEffect(() =>
-    this.dataPersistence.fetch(AuthActions.createToken, {
-      run: (action: ReturnType<typeof AuthActions.createToken>) => {
-        return this.service
-          .createToken({ username: action.username, password: action.password })
-          .pipe(
-            map(
-              token =>
-                AuthActions.createTokenSuccess({
-                  token,
-                  username: action.username
-                }) as any
-            )
-          );
-      },
+  createToken$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.createToken.type),
+      pessimisticUpdate({
+        run: (action: ReturnType<typeof AuthActions.createToken>) => {
+          return this.service
+            .createToken({
+              username: action.username,
+              password: action.password
+            })
+            .pipe(
+              map(
+                token =>
+                  AuthActions.createTokenSuccess({
+                    token,
+                    username: action.username
+                  }) as any
+              )
+            );
+        },
 
-      onError: (action: ReturnType<typeof AuthActions.createToken>, error) => {
-        console.error("Error", error);
-        return AuthActions.createTokenFailure({ error });
-      }
-    })
+        onError: (
+          action: ReturnType<typeof AuthActions.createToken>,
+          error
+        ) => {
+          console.error("Error", error);
+          return AuthActions.createTokenFailure({ error });
+        }
+      })
+    )
   );
 
   createTokenSuccess$: Observable<any> = createEffect(
     () =>
-      this.dataPersistence.fetch(AuthActions.createTokenSuccess, {
-        run: () => {
+      this.actions$.pipe(
+        ofType(AuthActions.createTokenSuccess.type),
+        tap(_ => {
           this.router.navigateByUrl("");
-        }
-      }),
-    { dispatch: false }
-  );
-
-  removeTokenSuccess$: Observable<any> = createEffect(
-    () =>
-      this.dataPersistence.fetch(AuthActions.removeTokenSuccess, {
-        run: () => {
-          document.location.reload();
-        }
-      }),
+        })
+      ),
     { dispatch: false }
   );
 
   removeToken$: Observable<any> = createEffect(() =>
-    this.dataPersistence.fetch(AuthActions.removeToken, {
-      run: () => {
-        this.service.removeToken();
-        return AuthActions.removeTokenSuccess();
-      },
+    this.actions$.pipe(
+      ofType(AuthActions.removeToken.type),
+      pessimisticUpdate({
+        run: () => {
+          this.service.removeToken();
+          return of(AuthActions.removeTokenSuccess());
+        },
 
-      onError: (action: ReturnType<typeof AuthActions.removeToken>, error) => {
-        console.error("Error", error);
-        return AuthActions.removeTokenFailure({ error });
-      }
-    })
+        onError: (
+          action: ReturnType<typeof AuthActions.removeToken>,
+          error
+        ) => {
+          console.error("Error", error);
+          return AuthActions.removeTokenFailure({ error });
+        }
+      })
+    )
+  );
+
+  removeTokenSuccess$: Observable<any> = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.removeTokenSuccess.type),
+        tap(_ => {
+          document.location.reload();
+        })
+      ),
+    { dispatch: false }
   );
 
   refreshToken$: Observable<any> = createEffect(() =>
-    this.dataPersistence.fetch(AuthActions.refreshToken, {
-      run: () => {
-        return this.service.refreshToken().pipe(
-          map(() => {
-            return AuthActions.refreshTokenSuccess();
-          })
-        );
-      },
+    this.actions$.pipe(
+      ofType(AuthActions.refreshToken.type),
+      pessimisticUpdate({
+        run: () => {
+          return this.service.refreshToken().pipe(
+            map(() => {
+              return AuthActions.refreshTokenSuccess();
+            })
+          );
+        },
 
-      onError: (action: ReturnType<typeof AuthActions.refreshToken>, error) => {
-        console.error("Error", error);
-        return AuthActions.refreshTokenFailure({ error });
-      }
-    })
+        onError: (
+          action: ReturnType<typeof AuthActions.refreshToken>,
+          error
+        ) => {
+          console.error("Error", error);
+          return AuthActions.refreshTokenFailure({ error });
+        }
+      })
+    )
   );
 
   // refreshTokenInterval$ = createEffect(() =>
@@ -117,7 +142,6 @@ export class AuthEffects {
   constructor(
     private actions$: Actions,
     @Optional() private service: AuthService,
-    private dataPersistence: DataPersistence<fromAuth.AuthPartialState>,
     private router: Router
   ) {}
 }
