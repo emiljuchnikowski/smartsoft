17a1a42d70f54f2485fd51f07671be9bb77ea603
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-24 21:21:02 +0200

    feature: Shared angular model without UI #32 - include facadies

--- libs/shared/angular/src/lib/utils-shared.module.ts
+++ libs/shared/angular/src/lib/utils-shared.module.ts
@@ -3,8 +3,10 @@ import { DeviceDetectorModule } from "ngx-device-detector";
 
 import { DynamicComponentLoader } from "./services/dynamic-component-loader/dynamic-component-loader.service";
 import { HardwareService } from "./services/hardware/hardware.service";
+import {SharedFactoriesModule} from "./factories/factories.module";
 
 @NgModule({
+  exports: [ SharedFactoriesModule ],
   imports: [DeviceDetectorModule, DeviceDetectorModule.forRoot()],
   providers: [DynamicComponentLoader, HardwareService]
 })
