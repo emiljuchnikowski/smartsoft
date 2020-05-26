4b33109dac3c8d19e4c5cf9a044a74d13b613a79
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-26 14:06:21 +0200

    module: Stream #26 - correct stream api name

--- apps/api/steam/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: 'api-steam',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/api/steam'
-};
---
+++ apps/api/stream/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'api-stream',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/api/stream'
+};
