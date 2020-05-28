80c0bfe2018f0bcbf34ddc8ddf7968265f63c696
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-29 00:33:08 +0200

    module: Stream #26 - refactor chart component

--- libs/shared/angular/src/lib/components/chart/chart.component.stories.ts
+++ libs/shared/angular/src/lib/components/chart/chart.component.stories.ts
@@ -3,28 +3,26 @@ import { ChartsModule } from "ng2-charts";
 import { ChartComponent } from "./chart.component";
 import { ChartType, IChartOptions } from "../../models/chart";
 import { ChartLineComponent } from "./line/line.component";
+import {storiesOf} from "@storybook/angular";
 
-export default {
-  title: "ChartComponent"
-};
-
-export const line = () => ({
-  moduleMetadata: {
-    imports: [ChartsModule],
-    declarations: [ChartLineComponent]
-  },
-  component: ChartComponent,
-  props: {
-    options: {
-      type: ChartType.line,
-      labels: ["January", "February", "March", "April", "May", "June"],
-      data: [{ values: [85, 72, 78, 75, 77, 75], label: "Crude oil prices" }],
-      colors: [
-        {
-          border: "black",
-          background: "rgba(255,255,0,0.28)"
-        }
-      ]
-    } as IChartOptions<any>
-  }
-});
+storiesOf("smart-char", module)
+    .add('line', () => ({
+      moduleMetadata: {
+        imports: [ChartsModule],
+        declarations: [ChartLineComponent]
+      },
+      component: ChartComponent,
+      props: {
+        options: {
+          type: ChartType.line,
+          labels: ["January", "February", "March", "April", "May", "June"],
+          data: [{ values: [85, 72, 78, 75, 77, 75], label: "Crude oil prices" }],
+          colors: [
+            {
+              border: "black",
+              background: "rgba(255,255,0,0.28)"
+            }
+          ]
+        } as IChartOptions<any>
+      }
+    }));
