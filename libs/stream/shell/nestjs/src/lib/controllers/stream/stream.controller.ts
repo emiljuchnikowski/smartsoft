873cd564311a6adb0ba33353d1adbb87cc0cbb31
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-30 20:51:53 +0200

    module: Stream #26 - get by id method in controller

--- libs/stream/shell/nestjs/src/lib/controllers/stream/stream.controller.ts
+++ libs/stream/shell/nestjs/src/lib/controllers/stream/stream.controller.ts
@@ -1,35 +1,58 @@
-import {Body, Controller, Delete, Param, Patch, Post, Res} from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Res
+} from "@nestjs/common";
 import { Response, Request } from "express";
 
-import {StreamService} from "@smartsoft001/stream-shell-app-services";
-import {IStreamCreate, IStreamUpdate} from "@smartsoft001/stream-domain";
+import { StreamService } from "@smartsoft001/stream-shell-app-services";
+import {
+  IStreamCreate,
+  IStreamUpdate,
+  Stream
+} from "@smartsoft001/stream-domain";
 
-@Controller('')
+@Controller("")
 export class StreamController {
-    constructor(private service: StreamService) {
-    }
+  constructor(private service: StreamService) {}
 
-    static getLink(req: Request): string {
-        return req.protocol + "://" + req.headers.host + req.url;
-    }
+  static getLink(req: Request): string {
+    return req.protocol + "://" + req.headers.host + req.url;
+  }
 
-    @Post()
-    async create(@Body() data: IStreamCreate, @Res() res: Response): Promise<{ id: string }> {
-        const id = await this.service.create(data);
-        res.set("Location", StreamController.getLink(res.req) + "/" + id);
-        return res.send({
-            id
-        });
-    }
+  @Get(":id")
+  getById(@Param() params: { id: string }): Promise<Stream> {
+    return this.service.getById(params.id);
+  }
 
-    @Patch(":id")
-    async update(@Param() params: { id: string }, @Body() data: IStreamUpdate): Promise<void> {
-        data.id = params.id;
-        await this.service.update(data);
-    }
+  @Post()
+  async create(
+    @Body() data: IStreamCreate,
+    @Res() res: Response
+  ): Promise<{ id: string }> {
+    const id = await this.service.create(data);
+    res.set("Location", StreamController.getLink(res.req) + "/" + id);
+    return res.send({
+      id
+    });
+  }
 
-    @Delete(":id")
-    async delete(@Param() params: { id: string }): Promise<void> {
-        await this.service.delete(params.id);
-    }
+  @Patch(":id")
+  async update(
+    @Param() params: { id: string },
+    @Body() data: IStreamUpdate
+  ): Promise<void> {
+    data.id = params.id;
+    await this.service.update(data);
+  }
+
+  @Delete(":id")
+  async delete(@Param() params: { id: string }): Promise<void> {
+    await this.service.delete(params.id);
+  }
 }
