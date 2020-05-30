dae4eafe41b45edb7a0555aa30f0bd9e6430237e
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-30 20:43:03 +0200

    module: Stream #26 - get by id method i app service

--- libs/stream/shell/app-services/src/lib/services/stream/stream.service.ts
+++ libs/stream/shell/app-services/src/lib/services/stream/stream.service.ts
@@ -1,13 +1,18 @@
 import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
 
-import {CreatorService, DeleteService, IStreamCreate, IStreamUpdate, UpdateService} from "@smartsoft001/stream-domain";
+import {
+    CreatorService, DeleteService, IStreamCreate, IStreamUpdate, UpdateService, Stream
+} from "@smartsoft001/stream-domain";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class StreamService {
     constructor(
         private readonly creatorService: CreatorService,
         private readonly updateService: UpdateService,
-        private readonly deleteService: DeleteService
+        private readonly deleteService: DeleteService,
+        @InjectRepository(Stream) private readonly repository: Repository<Stream>
     ) { }
 
     create(item: IStreamCreate): Promise<string> {
@@ -20,5 +25,9 @@ export class StreamService {
 
     delete(id: string): Promise<void> {
         return this.deleteService.delete(id);
+    }
+
+    getById(id: string): Promise<Stream> {
+        return this.repository.findOne({ id });
     }
 }
