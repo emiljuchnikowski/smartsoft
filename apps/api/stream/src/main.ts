b1f262a260a5972ec274af6fac81d265510f99a5
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-30 11:26:00 +0200

    module: Stream #26 - api configuration

--- apps/api/stream/src/main.ts
+++ apps/api/stream/src/main.ts
@@ -1,21 +1,24 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- **/
+import * as bodyParser from "body-parser";
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.port || 3333;
+  const port = process.env.port || 3334;
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.enableCors();
+
+  if (process.env.URL_PREFIX) {
+    app.setGlobalPrefix(process.env.URL_PREFIX)
+  }
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => {
-    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    console.log("Listening at http://localhost:" + port);
   });
 }
 
 bootstrap();
-    
