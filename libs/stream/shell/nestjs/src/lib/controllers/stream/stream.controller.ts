2ebd6b5ff953417c7efd9b2d07073184102071d2
Emil Juchnikowski <emil@juchnikowski.com>
2020-05-30 11:56:53 +0200

    module: Stream #26 - update controller method

--- libs/stream/shell/nestjs/src/lib/controllers/stream/stream.controller.ts
+++ libs/stream/shell/nestjs/src/lib/controllers/stream/stream.controller.ts
@@ -1,8 +1,8 @@
-import {Body, Controller, Post, Res} from "@nestjs/common";
+import {Body, Controller, Param, Patch, Post, Res} from "@nestjs/common";
 import { Response, Request } from "express";
 
 import {StreamService} from "@smartsoft001/stream-shell-app-services";
-import {IStreamCreate} from "@smartsoft001/stream-domain";
+import {IStreamCreate, IStreamUpdate} from "@smartsoft001/stream-domain";
 
 @Controller('')
 export class StreamController {
@@ -14,11 +14,17 @@ export class StreamController {
     }
 
     @Post()
-    async create(@Body() obj: IStreamCreate, @Res() res: Response): Promise<{ id: string }> {
-        const id = await this.service.create(obj);
+    async create(@Body() data: IStreamCreate, @Res() res: Response): Promise<{ id: string }> {
+        const id = await this.service.create(data);
         res.set("Location", StreamController.getLink(res.req) + "/" + id);
         return res.send({
             id
         });
+    }
+
+    @Patch(":id")
+    async update(@Param() params: { id: string }, @Body() data: IStreamUpdate): Promise<void> {
+        data.id = params.id;
+        await this.service.update(data);
     }
 }
