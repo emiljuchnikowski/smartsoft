069a8464205e6e06b1aeefefc654e35b456ab7ba
Emil Juchnikowski <emil@juchnikowski.com>
2020-06-25 21:48:03 +0200

    feature: PayPal support #34 - refactor app trans service

--- libs/trans/shell/app-services/src/lib/services/trans/trans.service.ts
+++ libs/trans/shell/app-services/src/lib/services/trans/trans.service.ts
@@ -1,38 +1,61 @@
-import {HttpService, Injectable, Optional} from "@nestjs/common";
+import { HttpService, Injectable, Optional } from "@nestjs/common";
 
-import {ITransCreate, Trans, TransConfig, CreatorService, RefresherService} from "@smartsoft001/trans-domain";
-import {PayuService} from "@smartsoft001/payu";
+import {
+  ITransCreate,
+  Trans,
+  TransConfig,
+  CreatorService,
+  RefresherService,
+  ITransPaymentSingleService,
+} from "@smartsoft001/trans-domain";
+import { PayuService } from "@smartsoft001/payu";
 
 @Injectable()
 export class TransService {
-    private _paymentService = {
-        'payu': this.payuService
-    };
+  private _paymentService: {
+    [key: string]: ITransPaymentSingleService;
+  } = {
+    payu: this.payuService,
+  };
 
-    private _internalService = {
-        create: (trans: Trans<any>) => {
-            return this.httpService.post(this.config.internalApiUrl, trans).toPromise().then(res => res.data);
-        },
+  private _internalService = {
+    create: (trans: Trans<any>) => {
+      return this.httpService
+        .post(this.config.internalApiUrl, trans)
+        .toPromise()
+        .then((res) => res.data);
+    },
 
-        refresh: (trans: Trans<any>) => {
-            return this.httpService.put(this.config.internalApiUrl + '/' + trans.id, trans).toPromise().then(res => res.data);
-        }
-    };
+    refresh: (trans: Trans<any>) => {
+      return this.httpService
+        .put(this.config.internalApiUrl + "/" + trans.id, trans)
+        .toPromise()
+        .then((res) => res.data);
+    },
+  };
 
-    constructor(
-        private creatorService: CreatorService<any>,
-        private refresherService: RefresherService<any>,
-        private httpService: HttpService,
-        private config: TransConfig,
-        @Optional() private payuService: PayuService
-    )
-    { }
+  constructor(
+    private creatorService: CreatorService<any>,
+    private refresherService: RefresherService<any>,
+    private httpService: HttpService,
+    private config: TransConfig,
+    @Optional() private payuService: PayuService
+  ) {}
 
-    create<T>(ops: ITransCreate<T>): Promise<string> {
-        return this.creatorService.create(ops, this._internalService, this._paymentService);
-    }
+  create<T>(ops: ITransCreate<T>): Promise<string> {
+    return this.creatorService.create(
+      ops,
+      this._internalService,
+      this._paymentService
+    );
+  }
 
-    async refresh(transId: string, data = {}): Promise<void> {
-        await this.refresherService.refresh(transId, this._internalService, this._paymentService, data);
-    }
+  async refresh(transId: string, data = {}): Promise<void> {
+    await this.refresherService.refresh(
+      transId,
+      this._internalService,
+      this._paymentService,
+      data
+    );
+  }
 }
