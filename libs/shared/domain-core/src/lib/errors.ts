439c39984f3317dee3f1823f6c8c641b267ef5f5
Emil Juchnikowski <emil@juchnikowski.com>
2020-09-11 12:36:12 +0200

    add type to error model in domain core

--- libs/shared/domain-core/src/lib/errors.ts
+++ libs/shared/domain-core/src/lib/errors.ts
@@ -1,10 +1,14 @@
 export class DomainValidationError extends Error {
+    type = DomainValidationError;
+
     constructor(msg) {
         super(msg);
     }
 }
 
 export class DomainForbiddenError extends Error {
+    type = DomainForbiddenError;
+
     constructor(msg) {
         super(msg);
     }
