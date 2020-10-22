13c281c084153b7dd0b89c961e447904285fbeb7
Emil Juchnikowski <emil@juchnikowski.com>
2020-10-22 22:52:52 +0200

    stream: libs refactoring

--- libs/stream/domain/jest.config.js
+++ libs/stream/domain/jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   name: "stream-domain",
   preset: "../../../jest.config.js",
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.spec.json",
+    },
+  },
+  testEnvironment: "node",
   transform: {
     "^.+\\.[tj]sx?$": "ts-jest",
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   coverageDirectory: "../../../coverage/libs/stream/domain",
-  globals: { "ts-jest": { tsConfig: "<rootDir>/tsconfig.spec.json" } },
 };
