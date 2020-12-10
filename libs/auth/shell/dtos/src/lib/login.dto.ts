a621bb6d5d1f5b730483ae5913e560ea265406fa
Emil Juchnikowski <emil@juchnikowski.com>
2020-12-10 06:59:53 +0100

    feature: Fingerprint #71 - set data

--- libs/auth/shell/dtos/src/lib/login.dto.ts
+++ libs/auth/shell/dtos/src/lib/login.dto.ts
@@ -1,28 +1,7 @@
-import {Field, FieldType, Model} from "@smartsoft001/models";
-
-@Model({})
-export class Test {
-    @Field({})
-    test = "asd"
-}
+import { Field, Model } from "@smartsoft001/models";
 
 @Model({})
 export class LoginDto {
-    @Field({ required: true, focused: true }) username: string;
-    @Field({ required: true }) password: string;
-
-    @Field({
-        type: FieldType.object,
-        classType: Test
-    }) test: Test;
-
-    @Field({})
-    test2 = "asd2"
-
-    constructor() {
-        this.test = new Test();
-    }
+  @Field({ required: true, focused: true }) username: string;
+  @Field({ required: true }) password: string;
 }
-
-
-
