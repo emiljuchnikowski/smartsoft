e6894bea9248b11131fa1a8b6c34bb87e77dedd2
Emil Juchnikowski <emil@juchnikowski.com>
2021-02-15 00:05:33 +0100

    fix(npm): Set correct npm modules versions

--- libs/shared/schematics/src/ng-add/index.ts
+++ libs/shared/schematics/src/ng-add/index.ts
@@ -19,7 +19,7 @@ function addPackageJsonDependencies(options: Schema): Rule {
         const dependencies: NodeDependency[] = [
             {
                 type: NodeDependencyType.Default,
-                version: '^0.0.11',
+                version: '^1.0.1',
                 name: '@smartsoft001/core',
             },
         ];
