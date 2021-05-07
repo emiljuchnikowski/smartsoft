f10e8afab736cf28fe98e1564dd82a11417ac320
Emil Juchnikowski <emil@juchnikowski.com>
2021-05-07 07:39:17 +0200

    feat(angular): Allow set css class when modal create
    Refs: #113

--- libs/shared/angular/src/lib/services/modal/modal.service.ts
+++ libs/shared/angular/src/lib/services/modal/modal.service.ts
@@ -5,17 +5,15 @@ import {ModalController} from "@ionic/angular";
 export class ModalService {
     constructor(private modalCtrl: ModalController) { }
 
-    async show(options: { component, props?, mode?: 'default' | 'bottom', cssClass?: string }): Promise<IModal> {
+    async show(options: { component, props?, mode?: 'default' | 'bottom', cssClass?: string[] }): Promise<IModal> {
         const modalOptions = {
             component: options.component,
             componentProps: options.props,
-            cssClass: options.cssClass
+            cssClass: options.cssClass ? options.cssClass : []
         } as any;
 
         if (options.mode === "bottom") {
-            modalOptions.cssClass =  [
-                'smart-modal-bottom'
-            ];
+            modalOptions.cssClass.push('smart-modal-bottom');
         }
 
         const modal = await this.modalCtrl.create(modalOptions);
