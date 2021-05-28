b5a888a9c397164c481c517e2277389913798ea7
Emil Juchnikowski <emil@juchnikowski.com>
2021-05-28 08:14:00 +0200

    feat(crud): export angular crud components

---
+++ libs/crud/shell/angular/src/lib/components/index.ts
@@ -0,0 +1,5 @@
+export * from "./export/export.component";
+export * from "./filter";
+export * from "./filters/filters.component";
+export * from "./filters-config/filters-config.component";
+export * from "./multiselect/multiselect.component";
