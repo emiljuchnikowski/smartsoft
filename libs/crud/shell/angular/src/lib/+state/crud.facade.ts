9d53a4fe43558123c0d5c33bd0b3ead98dcd6292
Emil Juchnikowski <emil@juchnikowski.com>
2021-06-26 09:29:13 +0200

    fix(angular): Static store in crud facade

--- libs/crud/shell/angular/src/lib/+state/crud.facade.ts
+++ libs/crud/shell/angular/src/lib/+state/crud.facade.ts
@@ -10,6 +10,7 @@ import { CrudConfig } from "../crud.config";
 import * as CrudActions from "./crud.actions";
 import * as CrudSelectors from "./crud.selectors";
 import {ICrudCreateManyOptions, ICrudFilter} from "../models/interfaces";
+import {NgrxStoreService} from "@smartsoft001/angular";
 
 
 @Injectable()
@@ -19,56 +20,85 @@ export class CrudFacade<T extends IEntity<string>> {
   list: Array<T>;
   filter: ICrudFilter;
 
-  loaded$: Observable<boolean> = this.store.pipe(
-    select(CrudSelectors.getCrudLoaded(this.config.entity))
-  );
-  loading$: Observable<boolean> = this.store.pipe(
-    select(CrudSelectors.getCrudLoaded(this.config.entity)),
-    map(l => !l)
-  );
-  selected$: Observable<T> = this.store.pipe(
-    select(CrudSelectors.getCrudSelected(this.config.entity)),
-    tap(s => {
-      this.selected = s;
-    })
-  );
-  multiSelected$: Observable<T[]> = this.store.pipe(
-      select(CrudSelectors.getCrudMultiSelected(this.config.entity)),
-      tap(s => {
-        this.multiSelected = s;
-      })
-  );
-  list$: Observable<T[]> = this.store.pipe(
-    select(CrudSelectors.getCrudList(this.config.entity)),
-    tap(l => {
-      this.list = l;
-    })
-  );
-  filter$: Observable<ICrudFilter> = this.store.pipe(
-    select(CrudSelectors.getCrudFilter(this.config.entity)),
-    tap(f => {
-      this.filter = f;
-    })
-  );
-  totalCount$: Observable<number> = this.store.pipe(
-    select(CrudSelectors.getCrudTotalCount(this.config.entity))
-  );
-  links$: Observable<any> = this.store.pipe(
-    select(CrudSelectors.getCrudLinks(this.config.entity))
-  );
+  get loaded$(): Observable<boolean> {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudLoaded(this.config.entity))
+    );
+  }
 
-  error$: Observable<any> = this.store.pipe(
-      select(CrudSelectors.getCrudError(this.config.entity))
-  );
+  get loading$(): Observable<boolean> {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudLoaded(this.config.entity)),
+        map(l => !l)
+    );
+  }
 
-  constructor(private store: Store<any>, private config: CrudConfig<T>) {}
+  get selected$(): Observable<T>  {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudSelected(this.config.entity)),
+        tap(s => {
+          this.selected = s;
+        })
+    );
+  }
+
+  get multiSelected$(): Observable<T[]> {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudMultiSelected(this.config.entity)),
+        tap(s => {
+          this.multiSelected = s;
+        })
+    );
+  }
+
+  get list$(): Observable<T[]> {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudList(this.config.entity)),
+        tap(l => {
+          this.list = l;
+        })
+    );
+  }
+
+  get filter$(): Observable<ICrudFilter> {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudFilter(this.config.entity)),
+        tap(f => {
+          this.filter = f;
+        })
+    );
+  }
+
+  get totalCount$(): Observable<number> {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudTotalCount(this.config.entity))
+    );
+  }
+
+  get links$(): Observable<any> {
+    return this.getStore().pipe(
+        select(CrudSelectors.getCrudLinks(this.config.entity))
+    );
+  }
+
+  get error$(): Observable<any> {
+    return  this.getStore().pipe(
+        select(CrudSelectors.getCrudError(this.config.entity))
+    );
+  }
+
+  constructor(private readonly store: Store<any>, private config: CrudConfig<T>) {
+    if (NgrxStoreService.store) {
+      this.store = NgrxStoreService.store;
+    }
+  }
 
   create(item: T): void {
-    this.store.dispatch(CrudActions.create(this.config.entity, item));
+    this.getStore().dispatch(CrudActions.create(this.config.entity, item));
   }
 
   createMany(items: Array<T>, options: ICrudCreateManyOptions): void {
-    this.store.dispatch(CrudActions.createMany(this.config.entity, { items, options }));
+    this.getStore().dispatch(CrudActions.createMany(this.config.entity, { items, options }));
   }
 
   read(filter: ICrudFilter = null): void {
@@ -82,38 +112,44 @@ export class CrudFacade<T extends IEntity<string>> {
       query: filter && filter.query ? filter.query : baseQuery
     };
 
-    this.store.dispatch(CrudActions.read(this.config.entity, fullFilter));
+    this.getStore().dispatch(CrudActions.read(this.config.entity, fullFilter));
   }
 
   select(id: string): void {
-    this.store.dispatch(CrudActions.select(this.config.entity, id));
+    this.getStore().dispatch(CrudActions.select(this.config.entity, id));
   }
 
   unselect(): void {
-    this.store.dispatch(CrudActions.unselect(this.config.entity));
+    this.getStore().dispatch(CrudActions.unselect(this.config.entity));
   }
 
   multiSelect(items: Array<T>): void {
-    this.store.dispatch(CrudActions.multiSelect(this.config.entity, items));
+    this.getStore().dispatch(CrudActions.multiSelect(this.config.entity, items));
   }
 
   update(item: T): void {
-    this.store.dispatch(CrudActions.update(this.config.entity, item));
+    this.getStore().dispatch(CrudActions.update(this.config.entity, item));
   }
 
   export(filter: ICrudFilter = null, format = null): void {
-    this.store.dispatch(CrudActions.exportList(this.config.entity, filter, format));
+    this.getStore().dispatch(CrudActions.exportList(this.config.entity, filter, format));
   }
 
   updatePartial(item: Partial<T> & { id: string }): void {
-    this.store.dispatch(CrudActions.updatePartial(this.config.entity, item));
+    this.getStore().dispatch(CrudActions.updatePartial(this.config.entity, item));
   }
 
   updatePartialMany(items: (Partial<T> & {id: string})[]) {
-    this.store.dispatch(CrudActions.updatePartialMany(this.config.entity, items));
+    this.getStore().dispatch(CrudActions.updatePartialMany(this.config.entity, items));
   }
 
   delete(id: string): void {
-    this.store.dispatch(CrudActions.deleteItem(this.config.entity, id));
+    this.getStore().dispatch(CrudActions.deleteItem(this.config.entity, id));
+  }
+
+  private getStore(): Store {
+    if (NgrxStoreService.store) return NgrxStoreService.store;
+
+    return this.store;
   }
 }
