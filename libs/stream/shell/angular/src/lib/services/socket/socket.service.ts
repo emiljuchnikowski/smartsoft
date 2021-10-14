aa04b29ac76e0b9eaf5192ec0119a6fd2207f756
Emil Juchnikowski <emil@juchnikowski.com>
2021-10-14 22:57:53 +0200

    refactor(angular): remove socket support

--- libs/stream/shell/angular/src/lib/services/socket/socket.service.ts
+++ libs/stream/shell/angular/src/lib/services/socket/socket.service.ts
@@ -1,17 +1,18 @@
-import {Socket} from "ngx-socket-io";
+//import {Socket} from "ngx-socket-io";
 import {Injectable} from "@angular/core";
 
 import {StreamConfig} from "../../stream.config";
 
 @Injectable()
-export class SocketService extends Socket {
-    constructor(private streamConfig: StreamConfig) {
-        super({
-            url: streamConfig.apiUrl,
-            options: {
-                transports: ["websocket"],
-                path: new URL(streamConfig.apiUrl).pathname + '/_socket/'
-            }
-        });
-    }
+export class SocketService //extends Socket
+{
+    // constructor(private streamConfig: StreamConfig) {
+    //     super({
+    //         url: streamConfig.apiUrl,
+    //         options: {
+    //             transports: ["websocket"],
+    //             path: new URL(streamConfig.apiUrl).pathname + '/_socket/'
+    //         }
+    //     });
+    // }
 }
