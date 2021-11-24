d162eb4384f27591daad88bcb30c91897cb220a5
Emil Juchnikowski <emil@juchnikowski.com>
2021-11-24 23:36:59 +0100

    build(core): manual build

--- libs/shared/core/src/lib/git-hooks/pre-push.js
+++ libs/shared/core/src/lib/git-hooks/pre-push.js
@@ -2,4 +2,5 @@ const osvar = process.platform;
 
 if (osvar === "darwin" || osvar === "win64") {
     require("openurl").open("https://yt.smartsoft.biz.pl/timesheets");
+    require("openurl").open("https://yt.smartsoft.biz.pl/agiles");
 }
