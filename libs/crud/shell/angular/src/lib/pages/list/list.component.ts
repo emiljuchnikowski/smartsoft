6303403acf46892625abd57d7652bc68107b3a63
Emil Juchnikowski <emil@juchnikowski.com>
2022-01-28 23:30:20 +0100

    fix(crud): Reload list with same filter

--- libs/crud/shell/angular/src/lib/pages/list/list.component.ts
+++ libs/crud/shell/angular/src/lib/pages/list/list.component.ts
@@ -134,7 +134,7 @@ export class ListComponent<T extends IEntity<string>>
             ? this.config.sort["default"] : null,
         sortDesc: this.searchService.filter?.sortDesc ? this.searchService.filter.sortDesc :  this.config.sort
             ? this.config.sort["defaultDesc"] : null,
-        query: this.searchService.filter?.query ? this.searchService : [],
+        query: this.searchService.filter?.query ? this.searchService.filter : [],
         ...this.searchService.filter
       };
     }
