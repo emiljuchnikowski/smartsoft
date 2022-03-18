457525c116d8851adb0d60dcdf83618ee24341e2
andrii.bilych <andrii@smartsoft.biz.pl>
2022-03-18 19:13:30 +0100

    feat(shared angular): loader height parameter and split panel label styles in the footer

--- libs/shared/angular/src/lib/components/loader/loader.component.ts
+++ libs/shared/angular/src/lib/components/loader/loader.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'smart-loader',
-    template: '<ion-spinner *ngIf="show"></ion-spinner>',
+    template: '<ion-spinner *ngIf="show" [style.height]="height"></ion-spinner>',
     styleUrls: [
         './loader.component.scss'
     ]
 })
 export class LoaderComponent {
     @Input() show: boolean;
+    @Input() height: string;
 }
