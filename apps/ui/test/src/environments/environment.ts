a63a5d001c909410edbb605923f2e755579503c2
Emil Juchnikowski <emil@juchnikowski.com>
2022-04-15 10:44:16 +0200

    chore: api sandbox

--- apps/ui/test/src/environments/environment.ts
+++ apps/ui/test/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //apiUrl: "http://5.189.160.99:9413/"
-  apiUrl: "http://207.180.210.142:1201/api/"
+  apiUrl: "https://sandbox.smartsoft.biz.pl/api/"
 };
