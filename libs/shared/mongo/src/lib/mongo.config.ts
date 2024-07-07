20e38f69259f7387ce0ccb8e75de3777f585033b
Emil Juchnikowski <emil@juchnikowski.com>
2024-07-07 18:09:23 +0200

    feat(shared): mongo url

--- libs/shared/mongo/src/lib/mongo.config.ts
+++ libs/shared/mongo/src/lib/mongo.config.ts
@@ -1,9 +1,10 @@
 export class MongoConfig {
-    host: string;
-    port: number;
+    host?: string;
+    port?: number;
     database: string;
     username?: string;
     password?: string;
     collection?: string;
+    url?: string;
     type?: any;
 }
